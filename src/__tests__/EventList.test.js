f659312edba5f7899ab8997e9c968efc77386b15
loonxdesign <hi@loon-design.de>
2024-02-14 22:03:27 +0100

    updated api.js

--- src/__tests__/EventList.test.js
+++ src/__tests__/EventList.test.js
@@ -1,6 +1,7 @@
-import { render } from '@testing-library/react';
-import EventList from '../components/EventList';
+import { render, within, waitFor } from '@testing-library/react';
 import { getEvents } from '../api';
+import EventList from '../components/EventList';
+import App from "../App";
 
 describe('<EventList /> component', () => {
   test('has an element with "list" role', () => {
@@ -12,5 +13,17 @@ describe('<EventList /> component', () => {
     const allEvents = await getEvents(); 
     const EventListComponent = render(<EventList events={allEvents} />);
 expect(EventListComponent.getAllByRole("listitem")).toHaveLength(allEvents.length);
+  });
+});
+
+describe('<EventList /> integration', () => {
+  test('renders a list of 32 events when the app is mounted and rendered', async () => {
+    const AppComponent = render(<App />);
+    const AppDOM = AppComponent.container.firstChild;
+    const EventListDOM = AppDOM.querySelector('#event-list');
+    await waitFor(() => {
+      const EventListItems = within(EventListDOM).queryAllByRole('listitem');
+      expect(EventListItems.length).toBe(32);
+    });
   });
 });
--- src/components/CitySearch.js
+++ src/components/CitySearch.js
@@ -1,6 +1,6 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 
-const CitySearch = ({ allLocations }) => {
+const CitySearch = ({ allLocations, setCurrentCity }) => {
   const [showSuggestions, setShowSuggestions] = useState(false);
   const [query, setQuery] = useState('');
   const [suggestions, setSuggestions] = useState([]);
@@ -21,7 +21,12 @@ const CitySearch = ({ allLocations }) => {
     const value = event.target.textContent;
     setQuery(value);
     setShowSuggestions(false); // to hide the list
+    setCurrentCity(value);
   };
+
+  useEffect(() => {
+    setSuggestions(allLocations);
+  }, [`${allLocations}`]);
 
   return (
     <div id="city-search">
@@ -33,19 +38,22 @@ const CitySearch = ({ allLocations }) => {
         onFocus={() => setShowSuggestions(true)}
         onChange={handleInputChanged}
       />
-      {showSuggestions ?
+      {showSuggestions ? (
         <ul className="suggestions">
           {suggestions.map((suggestion) => {
-            return <li onClick={handleItemClicked} key={suggestion}>{suggestion}</li>
+            return (
+              <li onClick={handleItemClicked} key={suggestion}>
+                {suggestion}
+              </li>
+            );
           })}
-          <li key='See all cities' onClick={handleItemClicked}>
+          <li key="See all cities" onClick={handleItemClicked}>
             <b>See all cities</b>
           </li>
         </ul>
-        : null
-      }
+      ) : null}
     </div>
- )
+  );
 };
 
 export default CitySearch;
